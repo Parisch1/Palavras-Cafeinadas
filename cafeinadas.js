2ceb0e9c903a83a3c31747a53b3f19090a006a84
Parisch1 <malu.parischi@gmail.com>
2024-05-15 12:09:33 -0300

    quinto commit

--- cafeinadas.js
+++ cafeinadas.js
@@ -37,72 +37,18 @@ function prevSlider(){
 btnNext.addEventListener('click', nextSlider);
 btnPrev.addEventListener('click', prevSlider);
 
+const menuItens = document.querySelector('.container-menu');
+const titulo = document.getElementById('titulo');
 
+function myFunction(x) {
+    x.classList.toggle("change");
 
-
-
-
-
-/*const carrossel = document.querySelector(".carrossel");
-const setasBtn = document.querySelectorAll(".container-carrossel .carrossel-btn");
-const larguraPainel1 = carrossel.querySelector(".painel").offsetWidth;
-const carrosselChildrens = [...carrossel.children];
-
-let arrastando = false, startX, startScrollLeft;
-
-let painelPerView = Math.round(carrossel.offsetWidth / larguraPainel1);
-
-carrosselChildrens.slice(-painelPerView).reverse().forEach(painel => {
-    carrossel.insertAdjacentHTML("afterbegin", painel.outerHTML);
-});
-
-
-carrosselChildrens.slice(0, painelPerView).forEach(painel => {
-    carrossel.insertAdjacentHTML("beforeend", painel.outerHTML);
-});
-
-
-setasBtn.forEach(btn => {
-    btn.addEventListener("click", () =>{
-        carrossel.scrollLeft += btn.id === "left" ? -larguraPainel1 : larguraPainel1;
-    })
-});
-
-
-const dragStart = (e) => {
-    arrastando = true;
-    carrossel.classList.add("dragging");
-    startX = e.pageX;
-    startScrollLeft = carrossel.scrollLeft;
-}
-
-const dragging = (e) => {
-    if(!arrastando) return;
-    carrossel.scrollLeft = startScrollLeft - (e.pageX - startX);
-}
-
-const dragStop = () => {
-    arrastando = false;
-    carrossel.classList.remove("dragging");
-}
-
-const infiniteScroll = () =>{
-  
-    if(carrossel.scrollLeft === 0){
-        carrossel.classList.add("no-transition");
-        carrossel.scrollLeft = carrossel.scrollWidth - ( 2 * carrossel.offsetWidth);
-        carrossel.classList.remove("no-transition");
-    } 
-    else{ 
-        if(Math.ceil(carrossel.scrollLeft) === carrossel.scrollWidth - carrossel.offsetWidth){
-            carrossel.classList.add("no-transition");
-            carrossel.scrollLeft = carrossel.offsetWidth;
-            carrossel.classList.remove("no-transition");
-        }
+    if(menuItens.style.display === 'block'){
+        menuItens.style.display = 'none';
+        titulo.style.display = 'block'
+    }
+    else{
+        menuItens.style.display = 'block';
+        titulo.style.display = 'none'
     }
 }
-
-carrossel.addEventListener("mousedown", dragStart);
-carrossel.addEventListener("mousemove", dragging);
-document.addEventListener("mouseup", dragStop);
-carrossel.addEventListener("scroll", infiniteScroll);*/
